ad2c20ff052acdd14ea7aebea2f98696e96a98d2
krobnson <krobnson@gmail.com>
2017-03-08 07:31:53 -0800

    create new template for article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
-	validates :title, presence: true, length: {minimum: 3, maximum: 50 }
-	validates :description, presense: true, length: {minimum: 10, maximum: 256 }
+#	validates :title, presence: true, length: {minimum: 3, maximum: 50 }
+#	validates :description, presense: true, length: {minimum: 10, maximum: 256 }
 	
 end
