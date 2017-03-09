d7e34887c4006a22d4176ef474dec1efd0f3f9c0
krobnson <krobnson@gmail.com>
2017-03-08 19:59:42 -0800

    link_to edit show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
 	
+	def index
+		@articles = Article.all
+	end
+	
 	def new
 		@article = Article.new
 	end
@@ -14,9 +18,24 @@ class ArticlesController < ApplicationController
 		end
 	end
 	
+	def edit
+		@article = Article.find(params[:id])
+		
+	end
+	
 	def show
 		@article = Article.find(params[:id])
 		
+	end
+	
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			flash[:notice] = "Your article was successfully updated"
+			redirect_to article_path(@article)
+		else
+			render 'edit'
+		end
 	end
 	
 	private
