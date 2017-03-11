dab4eab8b283d9dcae40f1ec66260cb09d1e94e8
krobnson <krobnson@gmail.com>
2017-03-10 19:33:23 -0800

    flash and errors

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,7 +11,7 @@ class ArticlesController < ApplicationController
 	def create
 		@article = Article.new(article_params)
 		if @article.save
-			flash[:notice] = "Your article was successfully created"
+			flash[:success] = "Your article was successfully created"
 			redirect_to article_path(@article)
 		else
 			render 'new'
@@ -31,14 +31,14 @@ class ArticlesController < ApplicationController
 	def destroy
 		
 		@article.destroy
-		flash[:notice] = "Article was successfully deleted"
+		flash[:danger] = "Article was successfully deleted"
 		redirect_to articles_path
 	end
 	
 	def update
 		
 		if @article.update(article_params)
-			flash[:notice] = "Your article was successfully updated"
+			flash[:success] = "Your article was successfully updated"
 			redirect_to article_path(@article)
 		else
 			render 'edit'
