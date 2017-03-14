5f1e32d591ddffafd1c138c2ac967bfba59c657c
krobnson <krobnson@gmail.com>
2017-03-13 19:36:40 -0700

    sessions and users coding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+	before_action :set_user, only: [:edit, :update, :show]
+	before_action :require_same_user, only: [:edit, :update]
 	
 	def index 
 		@users = User.paginate(page: params[:page], per_page: 3)
@@ -11,7 +13,7 @@ class UsersController < ApplicationController
 	def create 
 		@user = User.new(user_params)
 		if @user.save
-			flash[:success] = "Welcome to AB #{@user.username}"	
+			flash[:success] = "Welcome to Alpha Blog #{@user.username}"	
 			redirect_to articles_path
 		else
 			render 'new'
@@ -19,11 +21,10 @@ class UsersController < ApplicationController
 	end
 	
 	def edit
-		@user = User.find(params[:id])
+		
 	end
 	
 	def update 
-		@user = User.find(params[:id])
 		if @user.update(user_params)
 			flash[:success] = "Your account was updated succesfully."
 			redirect_to articles_path
@@ -33,14 +34,22 @@ class UsersController < ApplicationController
 	end
 	
 	def show 
-		@user = User.find(params[:id])
 		@user_articles = @user.articles.paginate(page: params[:page], per_page: 3)
 	end
 		
-	
 	private
 	def user_params
 		params.require(:user).permit(:username, :email, :password)
 	end
 	
+	def set_user
+		@user = User.find(params[:id])
+	end
+	
+	def require_same_user
+		if current_user != @user
+			flash[:danger] = "You can only edit your own account"
+			redirect_to root_path
+		end
+	end
 end
